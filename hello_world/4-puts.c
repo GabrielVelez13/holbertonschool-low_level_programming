997742928cb944b32b43aae6b461a71ddf08fd11
Gabriel Velez <gabrielvelez@Gabriels-MBP.localdomain>
2023-10-11 16:47:05 -0400

    figuring out betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
+/**
+* pus - outputs a string
+(*
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+
+int main(void)
+{
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
-	
-	return 0;
+
+	return (0);
 }
