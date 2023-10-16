817c635ed876b797a428d0ee382b4e555fea6827
Gabriel Velez <gabrielvelez@Gabriels-MBP.localdomain>
2023-10-16 13:34:11 -0400

    Testing task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - a for loop that prints the alphabet
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
