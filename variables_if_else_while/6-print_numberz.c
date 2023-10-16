3626f9d9c91f3c9ba4d15f8cdcffc0828f2ace05
Gabriel Velez <gabrielvelez@Gabriels-MBP.localdomain>
2023-10-16 13:42:52 -0400

    Testing task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - a for loop that prints numbers 0-9
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	for (int i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
