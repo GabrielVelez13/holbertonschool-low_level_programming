617293f1f1b3b11a4233f61fad6cb115d598a248
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-17 18:05:34 -0400

    testing task 9, a times table with 2 for loops

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* times_table - prints a times table till 9x9.
+*/
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int product;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = i * j;
+			if (j == 9)
+			{
+				printf("%2d$", product);
+			}
+			else
+			{
+				printf("%2d, ", product);
+			}
+		}
+		printf("\n");
+	}
+}
