718dbdf48cc6f7b85ae4829b8a54a6fb71809fa4
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-17 14:26:47 -0400

    testing task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_last_digit - gives the last digit of a number.
+*
+* @n: is the input parameter of function.
+*
+* Return: 1 if lowercase, 0 if not.
+*/
+
+int print_last_digit(int n)
+{
+	int x;
+
+	x = n % 10;
+
+	return (x);
+}
+
