a6517143214eb231c68363b047954114ed4d4a97
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-17 13:30:57 -0400

    testing task 3, fixed prototype and betty

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -9,17 +9,15 @@
 *
 * Return: 1 if lowercase, 0 if not.
 */
+
 int _islower(int c)
 {
-	if (c == 0 || c == 20)
-		{
-			return (0);
-		}
-	else if (c < 65 || c > 90)
-		{
-			return (1);
-		}
-	else {
+	if (islower(c) != 0)
+	{
+		return (1);
+	}
+	else
+	{
 		return (0);
 	}
 }
