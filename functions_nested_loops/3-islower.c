c2f68d46f408a12f2bebdd560e37e248898c6893
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-17 13:25:54 -0400

    testing task 3, added _putchar into main.h

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -9,15 +9,17 @@
 *
 * Return: 1 if lowercase, 0 if not.
 */
-
 int _islower(int c)
 {
-	if (islower(c) != 0)
-	{
-		return (1);
-	}
-	else
-	{
+	if (c == 0 || c == 20)
+		{
+			return (0);
+		}
+	else if (c < 65 || c > 90)
+		{
+			return (1);
+		}
+	else {
 		return (0);
 	}
 }
