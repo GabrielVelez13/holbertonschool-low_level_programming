bc13657196958a775f10644c4fcbd04c46c2ea35
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-16 23:05:24 -0400

    testing headers task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-* print_alphabet - a function that prints the alphabet 10 times
+* print_alphabet_x10 - a function that prints the alphabet 10 times
 *
 * Return: Always 0.
 */
@@ -12,7 +12,8 @@ void print_alphabet_x10(void)
 	int i;
 	char l;
 
-	for (i = 0; i <= 10; i++) {
+	for (i = 0; i < 10; i++) 
+	{
 		for (l = 'a'; l <= 'z'; l++)
 		{
 			putchar(l);
