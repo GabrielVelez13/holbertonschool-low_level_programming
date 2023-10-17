4247bfc766c826ceae6678f875c5b0c4b35e9657
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-16 22:44:46 -0400

    testing headers in c for the first time

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,20 +7,14 @@
 * Return: Always 0.
 */
 
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 void print_alphabet(void)
 {
 	char l;
-	
+
 	for (l = 'a'; l<='z'; l++)
-		{
-			putchar(l);
-		}
-	
+	{
+		putchar(l);
+	}
+
 	putchar('\n');
 }
