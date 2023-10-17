e2ed8c7979c6825e68f4baaffd0626fa33ff6c93
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-17 13:08:23 -0400

    testing task 3

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -14,10 +14,6 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (l = 'a'; l <= 'z'; l++)
-		{
-			putchar(l);
-		}
-		putchar('\n');
+		print_alphabet();
 	}
 }
---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+* _islower - checks if a word is lowercase or not.
+*
+* c - is the input parameter of function.
+*
+* Return: 1 if lowercase, 0 if not.
+*/
+
+int _islower(int c)
+{
+	if (islower(c) != 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
