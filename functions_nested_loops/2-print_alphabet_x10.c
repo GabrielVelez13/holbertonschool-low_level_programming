ef7a97d50d2cc11a44a65a88f1fb1af58e17d8bc
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-16 23:03:17 -0400

    testing headers in c for the first time, understood what the task asked of me

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_alphabet - a function that prints the alphabet 10 times
+*
+* Return: Always 0.
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char l;
+
+	for (i = 0; i <= 10; i++) {
+		for (l = 'a'; l <= 'z'; l++)
+		{
+			putchar(l);
+		}
+		putchar('\n');
+	}
+}
