07eb45c375cd3ebe60db8756460b5d7de0b1a956
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-17 23:20:18 -0400

    testing task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* add - adds two numbers.
+*
+* @x: is the input parameter of function.
+* @y: is the input parameter of function.
+*
+* Return: 1 if lowercase, 0 if not.
+*/
+
+int add(int x, int y)
+{
+	return (x + y);
+}
