b15f6c71627bd7424f658ed4679b4c5e8c02d345
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-17 23:36:42 -0400

    testing task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_to_98 - prints from given number still 98.
+*
+* @n: is the input parameter of function.
+*
+* Return: 1 if lowercase, 0 if not.
+*/
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d", i);
+			}
+		}
+	}
+
+	for (i = n; i <= 98; i++)
+	{
+		if (i != 98)
+		{
+			printf("%d, ", i);
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+}
