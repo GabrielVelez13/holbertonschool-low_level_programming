32b3178b315e01565643e1967103c053b40f0f49
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-21 23:41:31 -0400

    testing task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_square - print squares
+* @size: is the input parameter of function.
+*/
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
