e1ac5df64e27b4e97529647f090c1051389758c1
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-21 22:40:42 -0400

    Testing task 0 and 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _isdigit - checks if a int is a digit or not.
+*
+* @c: is the input parameter of function.
+*
+* Return: 1 if digit, 0 if not.
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef main_h
+#define main_h
+
+char _putchar(char r);
+int _isupper(int c);
+int _isdigit(int c);
+
+#endif
