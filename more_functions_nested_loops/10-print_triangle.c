edfd53a4f2610872b95d43aef729115bcbb1b41a
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-22 00:13:11 -0400

    Testing task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_triangle - print triangles
+* @size: is the input parameter of function.
+*/
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			if (size - i >= j)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
