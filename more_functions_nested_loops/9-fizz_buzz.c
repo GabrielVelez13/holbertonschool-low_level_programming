33c05326bbdb031427560b8d4c66aa528e957bf8
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-22 00:01:58 -0400

    Doing the FizzBuzz test

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+* main - does the FizzBuzz test.
+* Return: 0
+*/
+
+int main(void)
+{
+	int count = 1;
+
+	while (count <= 100)
+	{
+		if (count % 3 == 0 && count % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (count % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (count % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", count);
+		}
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
