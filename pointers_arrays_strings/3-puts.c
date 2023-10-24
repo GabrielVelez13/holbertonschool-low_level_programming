75bfb74391d04ae77a96453a2a46a1ebabdc6ba3
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-24 17:00:43 -0400

    Leaning how strings and pointers work. testting task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts - prints to stdout.
+* @str: parameters for the function.
+**/
+
+void _puts(char *str)
+{
+	fprintf(stdout, "%s\n", str);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ char _putchar(char r);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 
 #endif
