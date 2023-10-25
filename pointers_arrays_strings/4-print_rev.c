f3a10add161c0f97ba13f207f975e62ff43393d9
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-25 18:52:48 -0400

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* print_rev - prints a string in reverse.
+* @s: parameters for the function.
+**/
+
+void print_rev(char *s)
+{
+	int i, len;
+
+	len = strlen(s);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+}
