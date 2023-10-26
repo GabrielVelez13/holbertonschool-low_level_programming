03e9b40890f179277ff0348bb9cf7538342d97e9
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-25 23:25:29 -0400

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _strcpy - copies an array.
+* @dest: parameters for the function.
+* @src: parameters for the function.
+* Return: dest
+**/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
