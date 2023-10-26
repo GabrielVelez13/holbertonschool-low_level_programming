9691fcf754512b5c953c7ad032677e923734acd3
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-25 23:51:25 -0400

    task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
 #include "main.h"
 
 /**
@@ -10,16 +11,37 @@
 
 int _atoi(char *s)
 {
-	int i, res = 0;
+	int sign = 1, base = 0, i = 0;
 
 	if (s == 0)
 	{
 		return (0);
 	}
 
-	for (i = 0; s[i] != '\0'; ++i)
+	while (s[i] == ' ')
 	{
-		res = res * 10 + s[i] - '0';
+		i++;
 	}
-	return (res);
+
+	if (s[i] == '-' || s[i] == '+')
+	{
+		sign = 1 - 2 * (s[i++] == '-');
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (base > INT_MAX / 10 || (base == INT_MAX / 10 && s[i] - '0' > 7))
+		{
+			if (sign == 1)
+			{
+				return (INT_MAX);
+			}
+			else
+			{
+				return (INT_MIN);
+			}
+		}
+			base = 10 * base + (s[i++] - '0');
+	}
+	return (base * sign);
 }
