7f2aec5bc37ac0b5e69b6341c338c16e9de756e6
Gabriel Velez <gabrielvelez@Gabriels-MBP.localdomain>
2023-10-27 14:28:43 -0400

    task 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int ccs = 0;
+	char c1, c2;
 
-	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
-	ccs = (*s1 == *s2) ? 0 : (*s1 > *s2) ? 1 : -1;
-	return (ccs);
+	do {
+		c1 = *s1++;
+		c2 = *s2++;
+		if (c1 == '\0')
+		{
+			return (c1 - c2);
+		}
+	} while (c1 == c2);
+
+	return (c1 - c2);
 }
