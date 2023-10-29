db6f22c4e8392d23631892c575194cbd4dbede87
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-29 14:46:12 -0400

    testing task 6

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_chessboard - it prints a chess board.
+* @a: parameters for the function.
+**/
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if ((a[i][j] >= 'a' && a[i][j] <= 'z') || a[i][j] >= 'A' && a[i][j] <= 'Z')
+			{
+				printf("%c", a[i][j]);
+			}
+		}
+		putchar('\n');
+	}
+}
