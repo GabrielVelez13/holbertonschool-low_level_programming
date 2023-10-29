4ab2363cf91fcba290ea88c2cef04b24e9240d5b
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-29 14:13:13 -0400

    task 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strpbrk - locates the first occurance of accept in s.
+* @s: parameters for the function.
+* @accept: parameters for the function.
+* Return: s
+**/
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s)
+	{
+		char *p = accept;
+
+		for (; *p; p++)
+		{
+			if (*s == *p)
+			{
+				return (s);
+			}
+		}
+	}
+	return (NULL);
+}
