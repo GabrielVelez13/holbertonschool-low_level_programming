9b163d2399bc18dc967ef568cec12301d8134eaa
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-10-31 16:58:45 -0400

    Testing task 0, first time doing recursion.

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts_recursion - prints a string through recursion.
+* @s: parameter of the function.
+* Return: ends the recursion.
+**/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
