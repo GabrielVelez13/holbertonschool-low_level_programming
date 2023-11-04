73faaf496e54abd2f93efa883c53411849b962c2
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-04 00:06:16 -0400

    first time learning about argc and argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the name of the file.
+* @argc: parameters for the function.
+* @argv: parameters for the function.
+* Return: 0 (success)
+**/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
