0c33fff0d4899421d82b1e753725f43d5f173331
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-04 00:13:40 -0400

    testing task 1, print arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the name of the file.
+* @argc: parameters for the function.
+* @argv: parameters for the function.
+* Return: 0 (success)
+**/
+
+int main(int argc, char *argv[])
+{
+	char **x = argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
