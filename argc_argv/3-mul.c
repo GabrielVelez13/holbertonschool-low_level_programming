faade64d41d019f255598e8e4d76b5372ab3d0a4
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-04 00:28:43 -0400

    task 3, multiply with args

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* main - prints the name of the file.
+* @argc: parameters for the function.
+* @argv: parameters for the function.
+* Return: 0 (success)
+**/
+
+int main(int argc, char *argv[])
+{
+	int x = argc;
+	int product = atoi(argv[x]) * atoi(argv[x - 1]);
+
+	printf("%d\n", product);
+	return (0);
+}
