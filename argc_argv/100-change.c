aedd195d7cd6975c0d9884311aed52acb885418f
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-04 01:37:05 -0400

    testing challenge task, fixed bugs

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	if (cents == 0)
+	if (cents <= 0)
 	{
 		printf("0\n");
 		return (0);
@@ -41,19 +41,19 @@ int countCents(int cents)
 
 	while (cents != 0)
 	{
-		if (cents % 25 == 0)
+		if (cents >= 25)
 		{
 			cents -= 25;
 		}
-		else if (cents % 10 == 0)
+		else if (cents >= 10)
 		{
 			cents -= 10;
 		}
-		else if (cents % 5 == 0)
+		else if (cents >= 5)
 		{
 			cents -= 5;
 		}
-		else if (cents % 2 == 0)
+		else if (cents >= 2)
 		{
 			cents -= 2;
 		}
