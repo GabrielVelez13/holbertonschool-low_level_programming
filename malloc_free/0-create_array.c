f4b54e37fd930115d7a70286bb6355d0a3344751
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-08 18:51:39 -0400

    Firsttime learning about memory allocation

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,9 +13,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array = malloc(sizeof(char) * size);
 
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
 	if (size == 0)
 	{
-		array[0] = '\0';
 		return (NULL);
 	}
 
