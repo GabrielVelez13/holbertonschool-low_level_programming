14960cf7b1ffd02c2492ad035985c94601e350d7
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-08 19:07:32 -0400

    Testing task 2

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* _strdup - creates a new array that copies.
+* @str: a string.
+* Return: On success 0.
+* On error, NULL
+*/
+
+char *_strdup(char *str)
+{
+	int len;
+	char *newstr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+
+	newstr = malloc(len + 1);
+
+	if (newstr != NULL)
+	{
+		newstr = str;
+	}
+
+	return (newstr);
+}
