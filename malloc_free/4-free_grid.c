b5fc95639257d6f338eba54bd32bf9313a0cc45e
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-09 18:58:10 -0400

    Task 4, free memory giving a 3d array

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -6,11 +6,11 @@
 */
 void free_grid(int **grid, int height)
 {
+	int i;
+
 	if (grid != NULL)
 	{
-		int i;
-
-		for (i = 0; i < height; i++)
+		for (i = 0; i <= height; i++)
 		{
 			free(grid[i]);
 		}
