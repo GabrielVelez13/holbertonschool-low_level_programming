39173ae9a9ad18787c4ecbacb3b7df909acd9627
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-09 17:15:37 -0400

    Concatenate 2 strings with malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,15 +11,19 @@ char *str_concat(char *s1, char *s2)
 	char *strconcat;
 	int len1 = 0, len2 = 0, i = 0, j = 0;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	while (s1[len1])
+	if (s1 != NULL)
 	{
-		len1++;
+		while (s1[len1])
+		{
+			len1++;
+		}
 	}
-	while (s2[len2])
+	if (s2 != NULL)
 	{
-		len2++;
+		while (s2[len2])
+		{
+			len2++;
+		}
 	}
 	strconcat = malloc((sizeof(char) * (len1 + len2)) + 1);
 	if (strconcat == NULL)
--- malloc_free/testingMain2.c
+++ malloc_free/testingMain2.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
+
 char *str_concat(char *s1, char *s2);
 
-int main(int argc, char *argv[]) {
+int main(void)
+{
 	char *s;
 	
-	s = str_concat("Best ", "School");
+	s = str_concat("Hello", NULL);
 	if (s == NULL)
 		{
 			printf("failed\n");
@@ -18,6 +15,25 @@ int main(int argc, char *argv[]) {
 		}
 	printf("%s\n", s);
 	free(s);
+	
+	s = str_concat(NULL, "Hello");
+	if (s == NULL)
+		{
+			printf("failed\n");
+			return (1);
+		}
+	printf("%s\n", s);
+	free(s);
+	
+	s = str_concat(NULL, NULL);
+	if (s == NULL)
+		{
+			printf("failed\n");
+			return (1);
+		}
+	printf("%s\n", s);
+	free(s);
+	
 	return (0);
 }
 
@@ -26,17 +42,25 @@ char *str_concat(char *s1, char *s2)
 	char *strconcat;
 	int len1 = 0, len2 = 0, i = 0, j = 0;
 	
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	while (s1[len1])
+//	if (s1 == NULL && s2 == NULL)
+//		return (NULL);
+	if (s1 != NULL)
+	{
+		while (s1[len1])
 		{
 			len1++;
 		}
-	while (s2[len2])
+	}
+	
+	
+	if (s2 != NULL)
+	{
+		while (s2[len2])
 		{
 			len2++;
 		}
-	strconcat = malloc((sizeof(char) * (len1 + len2)) + 1);
+	}
+	strconcat = malloc((sizeof(char) * (len1 + len2 + 1)));
 	if (strconcat == NULL)
 		return (NULL);
 	while (i < len1)
