a7ff1538421f0a25d6f22e51d06c3d893e0edd5e
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-09 23:30:48 -0400

    task 1 concanate strings, fixed bugs

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,18 +12,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 
 	if (s1 != NULL)
+	{
 		while (s1[len1])
 		{
 			len1++;
 		}
+	}
 	if (s2 != NULL)
+	{
 		while (s2[len2])
 		{
 			len2++;
 		}
+	}
+
 	strconcat = malloc((sizeof(*strconcat) * (len1 + n + 1)));
+
 	if (strconcat == NULL)
 		return (NULL);
+
 	while (i < len1)
 	{
 		strconcat[i] = s1[i];
@@ -34,5 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		strconcat[i + j] = s2[j];
 		j++;
 	}
+
+	strconcat[i + j] = '\0';
 	return (strconcat);
 }
