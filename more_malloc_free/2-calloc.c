72016bb30805b9e31fcf2ef6f3f284c0aef7f129
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-10 09:09:11 -0400

    testing task 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *pointer;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	pointer = malloc(sizeof(size) * nmemb);
+
+	if (pointer == NULL)
+	{
+		return (NULL);
+	}
+
+	return (pointer);
+}
