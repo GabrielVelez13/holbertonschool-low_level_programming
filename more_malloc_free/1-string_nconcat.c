ed2b783de1712640e02a69fff6ebb64f874a5a37
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-09 23:16:08 -0400

    task 1 concanate strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -9,7 +9,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *strconcat;
-	int len1 = 0, len2 = 0, i = 0, j = 0;
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 
 	if (s1 != NULL)
 		while (s1[len1])
