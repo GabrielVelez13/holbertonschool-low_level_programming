9847ccb9db1a3f68f5f4f3e71333db9f9b115c9a
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-17 21:12:32 -0400

    task 4, create a new struct (using my own funcs for _strdup)

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,56 +1,6 @@
 #include "dog.h"
 
 /**
-* _strlen - gives the length of a string.
-* @str: a string
-* Return: the length of string
-*/
-int _strlen(char *str)
-{
-	int len = 0;
-
-	while (*str++)
-	{
-		len++;
-	}
-	return (len);
-}
-
-/**
-* _strcpy - copies a string to a new destination.
-* @dest: the destination
-* @src: a string to copy
-* Return: the copied string
-*/
-char *_strcpy(char *dest, char *src)
-{
-	while ((*dest++ = *src++))
-	{}
-
-	return (dest);
-}
-
-/**
-* _strdup - copies a string to a new destination.
-* @src: a string to duplicate
-* Return: the value to duplicate the string
-*/
-char *_strdup(char *src)
-{
-	int len = _strlen(src) + 1;
-	char *dup = malloc(len);
-	
-	if (dup == NULL)
-	{
-		free(dup);
-		return (NULL);
-	}
-	_strcpy(dup, src);
-
-	return (dup);
-}
-
-/**
 * new_dog - creates a new dog (struct)
 * @name: name of dog
 * @age: age of dog
