2c5a6c9a73aaa9baec4c5e4956584ad98a7ecf8f
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-17 21:28:50 -0400

    task 4, create a new struct changed sequencing of code and removed malloc from strdup

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,41 @@
 #include "dog.h"
 
 /**
+* _strlen - gives the length of a string.
+* @str: a string
+* Return: the length of string
+*/
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+* _strdup - copies a string to a new destination.
+* @dest: new destination.
+* @src: a string to duplicate
+* Return: the value to duplicate the string
+*/
+char *_strdup(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
 * new_dog - creates a new dog (struct)
 * @name: name of dog
 * @age: age of dog
@@ -15,28 +50,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	theOther = malloc(sizeof(theOther));
-
 	if (theOther == NULL)
 	{
 		free(theOther);
 		return (NULL);
 	}
 
-	theOther->name = _strdup(name);
+	theOther->name = malloc(sizeof(theOther->name) * (_strlen(name) + 1));
 	if (theOther->name == NULL)
 	{
 		free(theOther);
 		return (NULL);
 	}
 
-	theOther->owner = _strdup(owner);
-	if (theOther->name == NULL)
+	theOther->owner = malloc(sizeof(theOther->owner) * (_strlen(owner) + 1));
+	if (theOther->owner == NULL)
 	{
 		free(theOther->name);
 		free(theOther);
 		return (NULL);
 	}
+
+	theOther->name = _strdup(theOther->name, name);
 	theOther->age = age;
+	theOther->owner = _strdup(theOther->owner, owner);
 
 	return (theOther);
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -18,48 +18,6 @@ struct dog
 
 typedef struct dog dog_t;
 
-int _strlen(char *str)
-{
-	int len = 0;
-
-	while (*str++)
-	{
-		len++;
-	}
-	return (len);
-}
-
-/**
-* _strcpy - copies a string to a new destination.
-* @dest: the destination
-* @src: a string to copy
-* Return: the copied string
-*/
-char *_strcpy(char *dest, char *src)
-{
-	while ((*dest++ = *src++))
-	{}
-
-	return (dest);
-}
-
-/**
-* _strdup - copies a string to a new destination.
-* @src: a string to duplicate
-* Return: the value to duplicate the string
-*/
-char *_strdup(char *src)
-{
-	int len = _strlen(src) + 1;
-	char *dup = malloc(len);
-
-	if (dup != NULL)
-	{
-		_strcpy(dup, src);
-	}
-	return (dup);
-}
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
