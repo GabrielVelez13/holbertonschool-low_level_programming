018d6c3aef0830f5abfad80d8d86fd80fc659a62
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-18 23:23:01 -0400

    find index

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+* int_index - gives the index of the array where the number is found
+* @array: the array
+* @size: the size of the array
+* @cmp: a function to do in each element of the array
+* Return: the ind if found, -1 if error.
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0, ind = 0;
+
+	if (size <= 0 || !array || !size || !cmp)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			ind++;
+		}
+		else
+		{
+			return (ind);
+		}
+	}
+	return (-1);
+}
