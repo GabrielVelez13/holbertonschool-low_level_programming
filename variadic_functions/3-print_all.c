dd4ef2d2df417963c1b2f8180ebc9e2d0ecff440
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-19 18:03:26 -0400

    Task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,31 +11,35 @@ void print_all(const char * const format, ...)
 	va_list args;
 
 	va_start(args, format);
-
-	while (format[i])
+	if (format)
 	{
-		switch (format[i])
+		while (format[i])
 		{
-			case 'c':
-				printf("%c%s", va_arg(args, int), sep);
-				break;
-			case 'i':
-				printf("%d%s", va_arg(args, int), sep);
-				break;
-			case 'f':
-				printf("%f%s", va_arg(args, double), sep);
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (!str)
-				{
-					str = "(nil)";
-				}
-				printf("%s%s", str, sep);
-				break;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c%s", va_arg(args, int), sep);
+					break;
+				case 'i':
+					printf("%d%s", va_arg(args, int), sep);
+					break;
+				case 'f':
+					printf("%f%s", va_arg(args, double), sep);
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (!str)
+					{
+						str = "(nil)";
+					}
+					printf("%s%s", str, sep);
+					break;
+			}
+			sep = "";
+			i++;
 		}
-		i++;
 	}
 	printf("\n");
+
 	va_end(args);
 }
