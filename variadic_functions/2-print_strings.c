2a388870e344343a4200444c61719bf0ee3b67e1
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-19 17:18:22 -0400

    Task 1, print nums, fixed bugs

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+
+/**
+* print_strings - print them strings
+* @separator: the separator of the nums
+* @n: number of parameters
+* @...: all the numbers
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	char *str;
+	va_list args;
+
+	va_start(args, n);
+
+	if (separator != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			str = va_arg(args, char*);
+			if (str != NULL)
+			{
+				printf("%s%s", str, separator);
+			}
+			else
+			{
+				printf("(nil)");
+			}
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			str = va_arg(args, char*);
+			printf("%s", str);
+		}
+		printf("\n");
+	}
+
+	va_end(args);
+}
