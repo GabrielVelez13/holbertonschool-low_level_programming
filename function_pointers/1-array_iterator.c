78ba35ca8895267043212ec5b2c8810749295b69
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-18 21:35:57 -0400

    array iterator with pointer funcs

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - it iterates through an array for an action.
+* @array: the array
+* @size: the size of the array
+* @action: a function to do in each element of the array
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	if (!array || !size || !action)
+	{
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
