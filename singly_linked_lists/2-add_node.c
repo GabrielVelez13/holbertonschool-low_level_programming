01db68fe15f54f8f31a0a9998b0c60ef5da72dae
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-11-29 18:54:54 -0400

    task 2, add new links

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+* add_node - returns a new link added to list
+* @head: double pointer to the head of a linked list.
+* @str: a string
+* Return: new link
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	char *copy;
+	int len = 0;
+	list_t *temp = malloc(sizeof(list_t));
+
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+
+	copy = strdup(str);
+	if (copy == NULL)
+	{
+		free(temp);
+		return (NULL);
+	}
+
+	do {
+		len++;
+	} while (str[len] != '\0');
+
+	temp->str = copy;
+	temp->len = len;
+	temp->next = *head;
+
+	*head = temp;
+	return (temp);
+}
