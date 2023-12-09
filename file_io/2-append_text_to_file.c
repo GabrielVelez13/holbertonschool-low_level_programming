489a0e7fc356102912d9f599fa8761259ba9a8f5
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-12-09 00:29:29 -0400

    testing task 2, apending

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,5 +1,37 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(int argc, char *argv[]){
-	
+/**
+* create_file - creates a file with the data of text_content
+* @filename: points to the filename
+* @text_content: some text
+* Return: 1, or -1 if failed.
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int in, out, len = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+		{
+			len++;
+		}
+	}
+
+	in = open(filename, O_WRONLY | O_APPEND);
+	out = write(in, text_content, len);
+
+	if (out == -1)
+	{
+		return (-1);
+	}
+
+	close(in);
+
+	return (1);
 }
