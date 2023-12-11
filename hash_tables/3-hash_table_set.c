3fb288d4dba8db4422295d75e14d8f8f617f43cd
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-12-10 22:11:13 -0400

    testing task 4, can now traverse lists

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -21,7 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 	/*if key exists then update value*/
 	
-	if (strcmp(ht->array[i]->key, key) == 0)
+	if (strcmp(ht->array[index]->key, key) == 0)
 	{
 		free(ht->array[i]->value);
 		ht->array[i]->value = VCopy;
