f7f40b8f09170113fa81c4735847876308180f37
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2023-12-10 21:12:06 -0400

    fixing various bugs

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,13 +8,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+	int i;
 
-	ht = malloc(sizeof(*ht));
+	ht = malloc(sizeof(ht));
 	if (ht == NULL)
 		return (NULL);
 
 	ht->size = size;
-	ht->array = calloc(sizeof(hash_node_t *), ht->size);
+	ht->array = malloc(sizeof(hash_table_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+
+	for (i = 0; ht->array[i]; i++)
+		ht->array[i] = NULL;
 
 	return (ht);
 }
