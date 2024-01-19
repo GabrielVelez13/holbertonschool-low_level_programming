93f3dfdbc491708b22079d2b8599f09695b4e570
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2024-01-19 01:33:39 -0400

    adding linear search - fixed type bug

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -9,7 +9,7 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
-	int i = 0;
+	size_t i = 0;
 
 	if (array == NULL)
 		return (-1);
@@ -18,12 +18,12 @@ int linear_search(int *array, size_t size, int value)
 	{
 		if (array[i] == value)
 		{
-			printf("Value checked array[%d] = [%d]\n", i, array[i]);
+			printf("Value checked array[%zu] = [%d]\n", i, array[i]);
 			return (i);
 		}
 		else
 		{
-			printf("Value checked array[%d] = [%d]\n", i, array[i]);
+			printf("Value checked array[%zu] = [%d]\n", i, array[i]);
 		}
 	}
 	return (-1);
